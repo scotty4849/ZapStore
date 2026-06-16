1f3cf06f012aff461e287f95461e86483e42ea25
scotty4849 <49600042+scotty4849@users.noreply.github.com>
2026-06-16 06:10:25 -0400

    Update index.ts

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -1,4 +1,11 @@
-export * from "./users";
-export * from "./products";
-export * from "./tickets";
-export * from "./news_updates";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import * as schema from "./schema";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL must be set.");
+}
+
+export const pool = mysql.createPool(process.env.DATABASE_URL);
+export const db = drizzle(pool, { schema, mode: "default" });
+export * from "./schema";
